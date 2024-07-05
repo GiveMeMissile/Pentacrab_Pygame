877737187d5bd3fb303a98fdc0c652c00329e85a
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-07-05 14:50:23 -0500

    Update Pentacrab.py
    Added a visual and sound to the teleport. Though it still neeeds some polish.

--- Pentacrab.py
+++ Pentacrab.py
@@ -7,7 +7,8 @@ pygame.init()
 WIDTH = 1350
 HEIGHT = 650
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Background.png")), (WIDTH, HEIGHT))
+BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Background.png")),
+                                    (WIDTH, HEIGHT))
 FPS = 60
 
 # Player settings
@@ -19,42 +20,51 @@ velocity = 0
 ACCELERATION = 1
 MAX_VELOCITY = 6
 FRICTION = 0.1
+PLAYER_DIFFERENCE = 15
 
 TELEPORT_AMOUNT = 500
 TP_DELAY = 500
 TP_COOLDOWN = 5500
+TELEPORT_WIDTH, TELEPORT_HEIGHT = HITBOX_HEIGHT + PLAYER_DIFFERENCE, HITBOX_HEIGHT + PLAYER_DIFFERENCE
+TELEPORT_SYMBOL = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Teleport.png")),
+                                         (TELEPORT_WIDTH, TELEPORT_HEIGHT))
 
 X = WIDTH // 2
 Y = HEIGHT - HITBOX_HEIGHT
 HITBOX = pygame.Rect(X, Y, HITBOX_WIDTH, HITBOX_HEIGHT)
 
-PLAYER_RIGHT = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Player.png")), (HITBOX_WIDTH + 30, HITBOX_HEIGHT + 20))
+PLAYER_RIGHT = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Player.png")),
+                                      (HITBOX_WIDTH + 30, HITBOX_HEIGHT + 20))
 PLAYER_LEFT = pygame.transform.flip(PLAYER_RIGHT, flip_y=False, flip_x=True)
-PLAYER_JUMP_RIGHT = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Player_jump.png")),(HITBOX_WIDTH + 30, HITBOX_HEIGHT + 20))
+PLAYER_JUMP_RIGHT = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Player_jump.png")),
+                                           (HITBOX_WIDTH + 30, HITBOX_HEIGHT + 20))
 PLAYER_JUMP_LEFT = pygame.transform.flip(PLAYER_JUMP_RIGHT, flip_y=False, flip_x=True)
 
 # Sounds
 JUMP_SOUND = pygame.mixer.Sound("Pentacrab_Assets/Player_jump.mp3")
+TELEPORT_SOUND = pygame.mixer.Sound("Pentacrab_Assets/Teleport.mp3")
 
 # Platform settings
 PLATFORM_HEIGHT = 60
 PLATFORM_WIDTH = 200
 platforms = []
-PLATFORM_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Cloud_platfrom.png")), (PLATFORM_WIDTH, PLATFORM_HEIGHT))
+PLATFORM_IMAGE = pygame.transform.scale(pygame.image.load(os.path.join("Pentacrab_Assets", "Cloud_platfrom.png")),
+                                        (PLATFORM_WIDTH, PLATFORM_HEIGHT))
 PLATFORM = pygame.transform.rotate(pygame.transform.scale(PLATFORM_IMAGE, (PLATFORM_WIDTH, PLATFORM_HEIGHT)), 180)
 
-
-
 # Boss settings
-BOSS_WIDTH , BOSS_HEIGHT = 100, 100
+BOSS_WIDTH, BOSS_HEIGHT = 100, 100
 BOSS_Y = 100
 BOSS_HITBOX = pygame.Rect(X - 50, BOSS_Y, BOSS_WIDTH, BOSS_HEIGHT)
+
 
 def draw():
     WINDOW.blit(BACKGROUND, (0, 0))
     pygame.draw.rect(WINDOW, (255, 0, 255), BOSS_HITBOX)
     for platform_location_x, platform_location_y in platforms:
         WINDOW.blit(PLATFORM, (platform_location_x, platform_location_y))
+    for portal_hitbox in tp_hitbox:
+        WINDOW.blit(TELEPORT_SYMBOL, (portal_hitbox.x, portal_hitbox.y))
     if not Jump:
         if left:
             WINDOW.blit(PLAYER_LEFT, (HITBOX.x - 15, HITBOX.y - 15))
@@ -62,10 +72,11 @@ def draw():
             WINDOW.blit(PLAYER_RIGHT, (HITBOX.x - 15, HITBOX.y - 15))
     if Jump:
         if left:
-            WINDOW.blit(PLAYER_JUMP_LEFT, (HITBOX.x - 15, HITBOX.y - 15))
+            WINDOW.blit(PLAYER_JUMP_LEFT, (HITBOX.x - PLAYER_DIFFERENCE, HITBOX.y - PLAYER_DIFFERENCE))
         else:
-            WINDOW.blit(PLAYER_JUMP_RIGHT, (HITBOX.x - 15,HITBOX.y - 15 ))
+            WINDOW.blit(PLAYER_JUMP_RIGHT, (HITBOX.x - PLAYER_DIFFERENCE, HITBOX.y - PLAYER_DIFFERENCE))
     pygame.display.update()
+
 
 def setup_platforms():
     platform_location_x = 100
@@ -77,6 +88,7 @@ def setup_platforms():
             platform_location_y += PLATFORM_HEIGHT + 50
         else:
             platform_location_y -= PLATFORM_HEIGHT + 50
+
 
 def player_movements():
     global velocity, left
@@ -110,6 +122,7 @@ def player_movements():
         HITBOX.x = WIDTH - HITBOX_WIDTH
         velocity = 0
 
+
 def player_jump():
     global decent, Jump, initial_height, falling
     if decent == False:
@@ -136,6 +149,7 @@ def player_jump():
             Jump = False
             initial_height = HEIGHT - HITBOX_HEIGHT
 
+
 def gravity():
     global initial_height, falling, Jump
     if Jump == False:
@@ -154,7 +168,8 @@ def gravity():
         if falling == True:
             HITBOX.y += GRAVITY
 
-def teleport():
+
+def teleport_movement():
     global tele_up, tele_left, tele_right, tp_cooldown, tp_delay
 
     if tele_up and (current_time - tp_delay >= TP_DELAY):
@@ -180,9 +195,37 @@ def teleport():
 
     if tp_cooldown and (current_time - tp_delay >= TP_COOLDOWN):
         tp_cooldown = False
+
+
+def teleport_visual():
+    global tp_hitbox
+    dif = 0
+    if tele_up or tele_left or tele_right:
+        for _ in range(2):
+            dif += 1
+            if dif == 1:
+                portal_hitbox = pygame.Rect(HITBOX.x + PLAYER_DIFFERENCE - TELEPORT_WIDTH / 2,
+                                            HITBOX.y - PLAYER_DIFFERENCE, TELEPORT_WIDTH, TELEPORT_HEIGHT)
+            elif dif == 2:
+                if tele_up:
+                    portal_hitbox = pygame.Rect(HITBOX.x + PLAYER_DIFFERENCE - TELEPORT_WIDTH / 2,
+                                                HITBOX.y - PLAYER_DIFFERENCE - TELEPORT_AMOUNT, TELEPORT_WIDTH,
+                                                TELEPORT_HEIGHT)
+                elif tele_left:
+                    portal_hitbox = pygame.Rect(HITBOX.x + PLAYER_DIFFERENCE - TELEPORT_AMOUNT - TELEPORT_WIDTH / 2,
+                                                HITBOX.y + PLAYER_DIFFERENCE, TELEPORT_WIDTH, TELEPORT_HEIGHT)
+                elif tele_right:
+                    portal_hitbox = pygame.Rect(HITBOX.x + PLAYER_DIFFERENCE + TELEPORT_AMOUNT - TELEPORT_WIDTH / 2,
+                                                HITBOX.y - PLAYER_DIFFERENCE, TELEPORT_WIDTH, TELEPORT_HEIGHT)
+            tp_hitbox.append(portal_hitbox)
+    else:
+        tp_hitbox.clear()
+
+
 def main():
     global run, Jump, decent, falling, initial_height, tele_up, current_time, tp_delay, \
-        tele_left, tp_cooldown, tele_right, left
+        tele_left, tp_cooldown, tele_right, left, tp_hitbox
+    tp_hitbox = []
     tp_delay = 99999999
     left = False
     tp_cooldown = False
@@ -209,14 +252,17 @@ def main():
                     tele_up = True
                     tp_delay = current_time
                     tp_cooldown = True
+                    TELEPORT_SOUND.play()
                 if event.key == pygame.K_LEFT and not tp_cooldown:
                     tele_left = True
                     tp_delay = current_time
                     tp_cooldown = True
+                    TELEPORT_SOUND.play()
                 if event.key == pygame.K_RIGHT and not tp_cooldown:
                     tele_right = True
                     tp_delay = current_time
                     tp_cooldown = True
+                    TELEPORT_SOUND.play()
             if event.type == pygame.QUIT:
                 run = False
         falling = True
@@ -224,7 +270,8 @@ def main():
             gravity()
         else:
             player_jump()
-        teleport()
+        teleport_visual()
+        teleport_movement()
         player_movements()
         draw()
     pygame.quit()
